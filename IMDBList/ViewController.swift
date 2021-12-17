282d4ddb650e31bb6f910b9787fe2b921fca3cc7
Hernan <hernandario.va@gmail.com>
2021-12-17 08:46:07 +0100

    Initial Commit

---
+++ IMDBList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDBList
+//
+//  Created by Hernán Villamil on 17/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
