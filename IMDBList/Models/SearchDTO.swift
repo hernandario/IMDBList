8975d6baf0124e272487b1163f4bedd4230e829a
Hernan <hernandario.va@gmail.com>
2021-12-17 11:36:23 +0100

    chore: added search DTO

---
+++ IMDBList/Models/SearchDTO.swift
@@ -0,0 +1,39 @@
+//
+//  SearchDTO.swift
+//  IMDBList
+//
+//  Created by Hernán Villamil on 17/12/21.
+//
+
+import Foundation
+
+// MARK: - Search
+struct Search: Codable {
+    let search: [IMDBItem]
+    let totalResults, response: String
+
+    enum CodingKeys: String, CodingKey {
+        case search = "Search"
+        case totalResults
+        case response = "Response"
+    }
+}
+
+// MARK: - SearchElement
+struct IMDBItem: Codable {
+    let title, year, imdbID: String
+    let type: ItemType
+    let poster: String
+
+    enum CodingKeys: String, CodingKey {
+        case title = "Title"
+        case year = "Year"
+        case imdbID
+        case type = "Type"
+        case poster = "Poster"
+    }
+}
+
+enum ItemType: String, Codable {
+    case movie = "movie"
+}
